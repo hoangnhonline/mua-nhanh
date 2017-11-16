f8696a0ef158cc0a766c989aced804e23199e02c
HoangNH <hoangnhonline@gmail.com>
2017-11-16 20:29:08 +0700

    .

--- resources/views/frontend/pages/index.blade.php
+++ resources/views/frontend/pages/index.blade.php
@@ -1,32 +1,57 @@
 @extends('frontend.layout')
 @include('frontend.partials.meta')
 @section('content')
-<div class="block block-breadcrumb">
-    <div class="container">
-        <ul class="breadcrumb">
+<div class="container">
+    <div class="block-breadcrumb">
+        <ol class="breadcrumb">
             <li><a href="{!! route('home') !!}">Trang chủ</a></li>
             <li class="active">{!! $detailPage->title !!}</li>
-        </ul>
+        </ol>
     </div>
-</div><!-- /block-breadcrumb -->
-<div class="block block-two-col container">
-    <div class="row">
-        
-        <div class="col-sm-9 col-xs-12 block-col-main">
-            <div class="block-page-about">
-                <div class="block-page-common">
-                    <div class="block block-title">
-                        <h1 class="title-main">{!! $detailPage->title !!}</h1>
-                    </div>
-                </div>
-                <div class="block-article">
-                    <div class="block block-content block-editor-content">
-                        {!! $detailPage->content !!}
-                    </div>
-                </div>
-            </div>
-        </div><!-- /block-col-left -->
-        @include('frontend.cate.sidebar')
-    </div>
-</div><!-- /block_big-title -->
-@endsection  
+</div><!-- /.breadcrumb -->
+<main class="main-content clearfix">
+	<div class="container">
+		<div class="box box-shadow">
+			<div class="sidebar-content sidebar-first">
+				<div class="sidebar">
+				    <div class="sidebar_widget widget">
+				        <h3 class="widget_title">Giới thiệu</h3>
+				        <ul class="side-menu">
+				            <li><a href="#">Về Chúng Tôi</a></li>
+				            <li><a href="#">Quy chế hoạt động</a></li>
+				            <li><a href="#">Chính sách bảo mật thông tin</a></li>
+				            <li><a href="#">Cơ chế giải quyết tranh chấp</a></li>
+				            <li><a href="#">Liên Hệ</a></li>
+				        </ul>
+				    </div>
+				    <div class="sidebar_widget widget">
+				        <h3 class="widget_title">Trợ giúp</h3>
+				        <ul class="side-menu">
+				            <li><a href="#">Quy trình xử lý khi phát hiện hành vi kinh doanh vi phạm </a></li>
+				            <li><a href="#">Biện pháp xử lý khi phát hiện hành vi kinh doanh vi phạm </a></li>
+				            <li><a href="#">Cơ chế kiểm soát các nhà cung cấp</a></li>
+				            <li><a href="#">Chính sách giao hàng</a></li>
+				            <li><a href="#">Hotdeal E-voucher</a></li>
+				            <li><a href="#">Chính sách đổi trả hàng</a></li>
+				            <li><a href="#">Cách thức thanh toán</a></li>
+				        </ul>
+				    </div>
+				    <div class="sidebar_widget widget">
+				        <h3 class="widget_title">Hợp tác</h3>
+				        <ul class="side-menu">
+				            <li><a href="#">Hợp tác kinh doanh</a></li>
+				            <li class="active"><a href="#">Liên hệ hợp tác</a></li>
+				        </ul>
+				    </div>
+				</div>
+				<div class="content wysiwyg wysiwyg-news">
+				    <h1 class="page-title">{!! $detailPage->title !!}</h1>
+				    <div>
+				    	{!! $detailPage->content !!}
+				    </div>
+				</div>
+			</div>
+		</div>
+	</div>
+</main><!-- /.main -->
+@stop
