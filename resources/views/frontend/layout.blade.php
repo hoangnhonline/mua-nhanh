e298825dd4269c8da49fae709fd9c7c40299ab28
HoangNH <hoangnhonline@gmail.com>
2017-12-15 02:08:14 +0000

    new

--- resources/views/frontend/layout.blade.php
+++ resources/views/frontend/layout.blade.php
@@ -189,5 +189,14 @@
     <!-- ===== Js Common ===== -->
 	<script src="{{ URL::asset('public/assets/js/common.js') }}"></script>
 	@yield('js')
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-111231507-1"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+
+	  gtag('config', 'UA-111231507-1');
+	</script>
 </body>
 </html>
