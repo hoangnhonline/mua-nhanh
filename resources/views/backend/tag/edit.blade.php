15ab516058495151542a87867f1c536703ba1f05
HoangNH <mrhoang1989@gmail.com>
2017-12-27 15:16:23 +0700

    .

--- resources/views/backend/tag/edit.blade.php
+++ resources/views/backend/tag/edit.blade.php
@@ -41,9 +41,8 @@
               <div class="form-group">
                 <label for="email">Loại </label>
                 <select class="form-control" name="type" id="type">                                
-                  <option value="1" {{ 1 ==  old('type', $detail->type) ? "selected" : "" }}>BĐS</option>
-                  <option value="2" {{ 2 ==  old('type', $detail->type) ? "selected" : "" }}>Bài viết</option>
-                  <option value="3" {{ 3 ==  old('type', $detail->type) ? "selected" : "" }}>Tiện ích</option>
+                  <option value="1" {{ 1 ==  old('type', $detail->type) ? "selected" : "" }}>Sản phẩm</option>
+                  <option value="2" {{ 2 ==  old('type', $detail->type) ? "selected" : "" }}>Bài viết</option>                  
                 </select>
               </div>              
                <!-- text input -->
